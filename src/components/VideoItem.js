62ad2bd25beb1b6cf5466b0b6518a47204b21270
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-02 20:12:04 +0000

    Dipslaying video player and details and final optimisation.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,10 @@
 import './VideoItem.css'
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
-      <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} className="ui image" />
+    <div className="video-item item" onClick={() => onVideoSelect(video)}>
+      <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} className="ui image" />
       <div className="content">
         <div className="header">
           {video.snippet.title}
