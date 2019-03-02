9892c069f77fb99684d8b2f317357986b7c5ff4e
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-02 18:18:34 +0000

    Accessing API and fetching data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube'
 
 class App extends React.Component {
 
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    console.log(this.state);
+    this.setState({ videos: response.data.items });
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <div className="ui container"><SearchBar /></div>
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos.
+      </div>
     );
   }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
   onFormSubmit = e => {
     e.preventDefault();
 
-    // TODO: Make we call callback from parent component
+    this.props.onFormSubmit(this.state.term)
   }
 
   render() {
